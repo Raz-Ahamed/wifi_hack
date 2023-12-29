21ffc50347f7a3dfacd3e13b5316415308e6ddbd
Raz Ahamed <100101605+Raz-Ahamed@users.noreply.github.com>
2023-12-30 00:03:22 +0600

    Add files via upload

---
+++ file/password_maker.py
@@ -0,0 +1,13 @@
+#password Maker
+print()
+print()
+print()
+
+print('Password of how many digits?')
+print()
+
+a=input('Input Digits-:')
+if a == 'b':
+	print('Wrong Input please Input Only Numbe')
+
+print(a)
---
+++ file/update.py
@@ -0,0 +1,4 @@
+import os 
+os.system('clear')
+os.system('figlet -f big Wifi - Hack |lolcat')
+os.system('sudo python wifite.py -i wlan0 -K')
