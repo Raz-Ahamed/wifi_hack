9f640008aaf30830e708c38c06702b9906c4d743
Raz Ahamed <100101605+Raz-Ahamed@users.noreply.github.com>
2026-07-04 23:43:56 +0600

    Create wpa.py

---
+++ file/wpa.py
@@ -0,0 +1,54 @@
+#python 
+#Youtube chanel name: Real tips & trikes 660
+#wifi_hack 
+#wps_hack 
+#start 
+import os
+import time
+print('Chack Update')
+os.system('git pull')
+os.system('clear')
+reset="\033[0m"         # Reset/Default Color
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+os.system('clear')
+os.system('figlet -f big Wifi - Hack |lolcat')
+print(green+'                     Version : 3.0 ')
+print()
+print(blue+'======================================================')
+print(red+'Note: I wont be responsible fo any illigal activites.')
+print(blue+'======================================================')
+print()
+time=1
+#main.coding
+#________________________________________
+import os
+import sys
+import time
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+delay_print(green+'%37s' % '   |Devoloped By : Raz Ahamed|\n')
+print()
+print(red + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + reset)
+print(blue + 'Youtube Chanel: Real  tips and Trikes 660' + reset)
+print(blue + 'Telegarm Grop : https://t.me/realtips6600' + reset)
+print(red + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + reset)
+print(green+'Root chack.......'+ reset)
+os.system('sudo python3 file/wps.py -i wlan0 -K')
+are=str(input(cyan+'Are you continue (y/n):- '))
+if are=='y':
+	os.system('python3 wifi.py')
+elif are=='n':
+	os.system('exit')
+	os.system('clear')
+	os.system('figlet -f big Thanks |lolcat')
+  os.system("exit")
